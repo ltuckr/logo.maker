50e7b2e19ce4193c27cb686a4a0d76af8e50b4b7
ltuckr <l.tucker.rdh@gmail.com>
2023-06-25 21:05:00 -0400

    added write function using responses

--- index.js
+++ index.js
@@ -3,5 +3,16 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 
 //shape library
-const {Square, Circle } = require("./lib/shapes");
+const {Square, Circle, Triangle } = require("./lib/shapes");
+
+// Function writes the SVG file using user answers from inquirer prompts
+function writeToFile(fileName, answers) {
+    let svgString = "";
+    // Logo size
+    svgString =
+      '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
+    // text placed inside shape
+    svgString += "<g>";
+    // Input shape selection into SVG
+    svgString += `${answers.shape}`;
 
