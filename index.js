0a37433378b396108e31fd6431c4673f30d020c3
ltuckr <l.tucker.rdh@gmail.com>
2023-06-26 07:17:21 -0400

    added text string detailing

--- index.js
+++ index.js
@@ -28,4 +28,8 @@ function writeToFile(fileName, answers) {
     shapeChoice = new Circle();
     svgString += `<circle cx="150" cy="115" r="80" fill="${answers.shapeBackgroundColor}"/>`;
   }
+// text design 
+  svgString += `<text x="150" y="130" text-anchor="middle" font-size="40" fill="${answers.textColor}">${answers.text}</text>`;
+  svgString += "</g>";
+  svgString += "</svg>";
 
